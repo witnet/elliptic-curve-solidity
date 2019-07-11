78de5929abff4aa0cba18e0582aa71737211d6a7
Mario Cao <mario@stampery.co>
2019-07-11 11:55:55 +0200

    chore: add solidity coverage

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,13 @@ module.exports = {
       port: 8545,
       network_id: "*",
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
   },
   mocha: {
     // timeout: 100000
