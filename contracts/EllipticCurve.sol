094eb3c34f3b8b9cc175ae5eb3424c7b40c8bab2
Claudia Bartoli Duncan <claudia@stampery.co>
2019-08-27 15:10:05 +0200

    add: scalar_decom_solidity
    Signed-off-by: Claudia Bartoli Duncan <claudia@stampery.co>

--- contracts/EllipticCurve.sol
+++ contracts/EllipticCurve.sol
@@ -9,7 +9,6 @@ pragma solidity ^0.5.0;
 
 contract EllipticCurve {
 
-  uint256 constant fieldOrder = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
   /// @dev Modular euclidean inverse of a number (mod p)
   /// @param x The number
   /// @param pp The modulus
@@ -221,6 +220,62 @@ contract EllipticCurve {
       pp);
   }
 
+  /// @dev Decomposition of the scalar k in two scalars k1 and k2 with half bit-length, such that k=k1+k2*LAMBDA (mod n)
+  /// @param _k the scalar to be decompose
+  /// @param _pp the modulus
+  /// @param _LAMBDA is a root of the characteristic polynomial of an endomorphism of the curve
+  /// @return k1 and k2  such that k=k1+k2*LAMBDA (mod n)
+  function scalarDecomposition (uint256 _k, uint256 _pp, uint256 _LAMBDA) public pure returns (int256[2] memory) {
+  // Extended Euclidean Algorithm for n and LAMBDA
+    int256 t = 1;
+    int256 oldt = 0;
+    uint256 r = uint256(_LAMBDA);
+    uint256 oldr = uint256(_pp);
+    uint256 quotient;
+
+    while (uint256(r) >= sqrt(_pp)) {
+      quotient = oldr / r;
+      (oldr, r) = (r, oldr - quotient*r);
+      (oldt, t) = (t, oldt - int256(quotient)*t);
+    }
+  // the vectors v1=(a1, b1) and v2=(a2,b2)
+    int256[4] memory ab;
+    ab[0] = int256(r);
+    ab[1] = int256(0 - t);
+    ab[2] = int256(oldr);
+    ab[3] = 0-oldt;
+
+  //b2*K
+    uint[3] memory test;
+    (test[0],test[1], test[2]) = multiply256(uint(ab[3]), uint(_k));
+
+  //-b1*k
+    uint[3] memory test2;
+    (test2[0], test2[1], test2[2]) = multiply256(uint(-ab[1]), uint(_k));
+  //c1 and c2
+    uint[2] memory c1;
+    (c1[0],c1[1]) = bigDivision(uint256 (uint128 (test[0])) << 128 | uint128 (test[1]), uint256(test[2]) + (_pp / 2), _pp);
+
+    uint[2] memory c2;
+    (c2[0],c2[1]) = bigDivision(uint256 (uint128 (test2[0])) << 128 | uint128 (test2[1]), uint256(test2[2]) + (_pp / 2), _pp);
+
+  // the decomposition of k in k1 and k2
+    int256 k1 = int256((int256(_k) - int256(c1[0]) * int256(ab[0]) - int256(c2[0]) * int256(ab[2])) % int256(_pp));
+    int256 k2 = int256((-int256(c1[0]) * int256(ab[1]) - int256(c2[0]) * int256(ab[3])) % int256(_pp));
+    if (uint256(abs(k1)) <= (_pp / 2)) {
+      k1 = k1;
+    } else {
+      k1 = int256(uint256(k1) - _pp);
+    }
+    if (uint256(abs(k2)) <= (_pp / 2)) {
+      k2 = k2;
+    } else {
+      k2 = int256(uint256(k2) - _pp);
+    }
+
+    return [k1, k2];
+  }
+
   /// @dev Multiply point (x1, y1, z1) times d in affine coordinates.
   /// @param d scalar to multiply
   /// @param x coordinate x of P1
@@ -251,7 +306,7 @@ contract EllipticCurve {
       z1,
       pp);
   }
-
+  
   function _lookup_sim_mul(
     uint256[3][4][4] memory iP, 
     uint256[4] memory P_Q,
@@ -260,14 +315,14 @@ contract EllipticCurve {
   ) internal pure {
     uint256 p = fieldOrder;
     uint256 beta = 0x7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501ee;
-    
+
     uint256[3][4] memory iPj;
     uint256[3] memory double;
 
     // P1 Lookup Table
     iPj = iP[0];
     iPj[0] = [P_Q[0], P_Q[1], 1];  						// P1
-        
+
     (double[0], double[1], double[2]) = jacDouble(iPj[0][0], iPj[0][1], 1, a, pp);
     (iPj[1][0], iPj[1][1], iPj[1][2]) = jacAdd(double[0], double[1], double[2], iPj[0][0], iPj[0][1], iPj[0][2], pp);
     (iPj[2][0], iPj[2][1], iPj[2][2]) = jacAdd(double[0], double[1], double[2], iPj[1][0], iPj[1][1], iPj[1][2], pp);
@@ -301,7 +356,7 @@ contract EllipticCurve {
   /// @param d A 256-bit integer
   /// @return (ptr, length) The pointer to the first coefficient, and the total length of the array
   function _wnaf(int256 d) internal pure  returns (uint256 ptr, uint256 length) {
-    
+
     int sign = d < 0 ? -1 : int(1);
     uint256 k = uint256(sign * d);
 
@@ -469,7 +524,7 @@ contract EllipticCurve {
     uint256 y2,
     uint256 z2,
     uint256 pp)
-  internal pure  returns (uint256 qx, uint256 qy, uint256 qz)
+  internal pure returns (uint256 qx, uint256 qy, uint256 qz)
   {
     if ((x1==0)&&(y1==0))
       return (x2, y2, z2);
@@ -568,7 +623,7 @@ contract EllipticCurve {
     uint256 z,
     uint256 a,
     uint256 pp)
-  internal pure  returns (uint256 qx, uint256 qy, uint256 qz)
+  internal pure returns (uint256 qx, uint256 qy, uint256 qz)
   {
     uint256 remaining = d;
     uint256[3] memory point;
@@ -602,5 +657,87 @@ contract EllipticCurve {
         a,
         pp);
     }
+  }
+
+
+  /// @dev Multiplication of a uint256 a and uint256 b. Because in Solidity each variable can not be greater than 256 bits,
+  /// this function separates the result of the multiplication in three parts, so the result would be the concatenation of those three
+  /// @param _a uint256
+  /// @param _b uint256
+  /// @return (ab2, ab1, ab0)
+  function multiply256(uint256 _a, uint256 _b) internal pure returns (uint256, uint256, uint256) {
+    uint256 aM = _a >> 128;
+    uint256 am = _a & 2**128-1;
+    uint256 bM = _b >> 128;
+    uint256 bm = _b & 2**128-1;
+    uint256 ab0 = am * bm;
+    uint256 ab1 = (ab0 >> 128) + (aM * bm & 2**128-1) + (am * bM & 2**128 - 1);
+    uint256 ab2 = (ab1 >> 128) + aM * bM + (aM * bm >> 128) + (am * bM >> 128);
+    ab1 &= 2**128 - 1;
+    ab0 &= 2**128 - 1;
+
+    return (ab2, ab1, ab0);
+  }
+
+  /// @dev Division of an integer of 312 bits by a 256-bit integer
+  /// @param _aM the higher 256 bits of the numarator
+  /// @param _am the lower 128 bits of the numarator
+  /// @param _b the 256-bit denominator
+  /// @return q the result of the division and the rest r
+  function bigDivision(uint256 _aM, uint256 _am, uint256 _b) internal pure returns (uint256, uint256) {
+    uint256 qM = (_aM / _b) << 128;
+    uint256 aM = _aM % _b;
+
+    uint256 shift = 0;
+    while (_b >> shift > 0) {
+      shift++;
+    }
+    shift = 256 - shift;
+    aM = (_aM << shift) + (shift > 128 ? _am << (shift - 128) : _am >> (128 - shift));
+    uint256 a0 = (_am << shift) & 2**128-1;
+    uint256 b = _b << shift;
+    (uint256 b1, uint256 b0) = (b >> 128, b & 2**128-1);
+
+    uint256 rM;
+    uint256 q = aM / b1;
+    rM = aM % b1;
+
+    uint256 rsub0 = (q & 2**128-1) * b0;
+    uint256 rsub21 = (q >> 128) * b0 + (rsub0 >> 128);
+    rsub0 &= 2**128-1;
+
+    while (rsub21 > rM || rsub21 == rM && rsub0 > a0) {
+      q--;
+      a0 += b0;
+      rM += b1 + (a0 >> 128);
+      a0 &= 2**128-1;
+    }
+
+    q += qM;
+    uint256 r = (((rM - rsub21) << 128) + _am - rsub0) >> shift;
+
+    return (q, r);
+  }
+
+  /// @dev Sqare root of an 256-bit integer
+  /// @param _x the integer
+  /// @return y the square root of _x
+  function  sqrt(uint256 _x) internal pure returns (uint256) {
+    uint256 z = (_x + 1) / 2;
+    uint256 y = _x;
+    while (z < y) {
+      y = z;
+      z = (_x / z + z) / 2;
+    }
+    return (y);
+  }
+
+  /// @dev Absolute value of a 25-bit integer
+  /// @param _x the integer
+  /// @return _x if _x>=0 or -_x if not
+  function abs(int256 _x) internal pure returns (int256) {
+    if (_x >= 0)
+    return _x;
+    return -_x;
   }
 }
